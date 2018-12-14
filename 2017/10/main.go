8c697f72706cffc108dfbabb391259ef44cfdc7b
mat285 <mat285@cornell.edu>
2018-12-13 20:44:48 -0800

    started 17-10

--- 2017/10/main.go
+++ 2017/10/main.go
@@ -1,14 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"encoding/hex"
+	"fmt"
+)
 
-var lengths = []int{106, 118, 236, 1, 130, 0, 235, 254, 59, 205, 2, 87, 129, 25, 255, 118}
+var lengths = []byte{106, 118, 236, 1, 130, 0, 235, 254, 59, 205, 2, 87, 129, 25, 255, 118}
+var extra = []byte{17, 31, 73, 47, 23}
 
 func main() {
 	part1(duplicate(lengths))
+	part2()
 }
 
-func part1(input []int) {
+func part1(input []byte) {
 	r := newRing(iter(256))
 	for _, i := range input {
 		r.act(i)
@@ -16,10 +21,32 @@ func part1(input []int) {
 	fmt.Println(int(r.buf[0]) * int(r.buf[1]))
 }
 
+func part2() {
+	fmt.Println(Hash(""))
+}
+
+func asString(input []byte) string {
+	s := ""
+	for _, i := range input {
+		s += fmt.Sprintf("%d,", i)
+	}
+	return s[:len(s)-1]
+}
+
 type ring struct {
 	buf      []byte
 	pos      int
 	skipSize int
+}
+
+func Hash(s string) string {
+	r := newRing(iter(256))
+	ls := []byte(s)
+	ls = append(ls, extra...)
+	for i := 0; i < 64; i++ {
+		r.round(ls)
+	}
+	return hex.EncodeToString(r.dense())
 }
 
 func newRing(buf []byte) *ring {
@@ -30,23 +57,42 @@ func newRing(buf []byte) *ring {
 	}
 }
 
+func (r *ring) dense() []byte {
+	ret := make([]byte, 16)
+	for i := 0; i < len(r.buf); i++ {
+		if i%16 == 0 {
+			ret[i%16] = r.buf[i]
+		} else {
+			idx := i / 16
+			ret[idx] ^= r.buf[i]
+		}
+	}
+	return ret
+}
+
+func (r *ring) round(ls []byte) {
+	for _, l := range ls {
+		r.act(l)
+	}
+}
+
 func (r *ring) skip(i int) {
 	r.pos = (r.pos + i) % len(r.buf)
 }
 
-func (r *ring) reverse(l int) {
-	for i := 0; i < l/2; i++ {
+func (r *ring) reverse(l byte) {
+	for i := 0; i < int(l)/2; i++ {
 		start := (r.pos + i) % len(r.buf)
-		end := (r.pos + l - i - 1) % len(r.buf)
+		end := (r.pos + int(l) - i - 1) % len(r.buf)
 		v1 := r.buf[start]
 		r.buf[start] = r.buf[end]
 		r.buf[end] = v1
 	}
 }
 
-func (r *ring) act(l int) {
+func (r *ring) act(l byte) {
 	r.reverse(l)
-	r.skip(l + r.skipSize)
+	r.skip(int(l) + r.skipSize)
 	r.skipSize++
 }
 
@@ -58,8 +104,8 @@ func iter(l int) []byte {
 	return ret
 }
 
-func duplicate(i []int) []int {
-	dst := make([]int, len(i))
+func duplicate(i []byte) []byte {
+	dst := make([]byte, len(i))
 	copy(dst, i)
 	return dst
 }
