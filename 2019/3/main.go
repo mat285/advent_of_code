123c03e9a98e029ce5c39a9b8db4027c87073f01
mat285 <mat285@cornell.edu>
2019-12-03 00:22:47 -0500

    2019 day 3

---
+++ 2019/3/main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
+
+	part1(lines)
+	part2(lines)
+}
+
+func part1(lines []string) {
+	w1 := wire(lines[0])
+	w2 := wire(lines[1])
+
+	ins := intersect(w1, w2)
+
+	min := math.MaxInt32
+	for _, i := range ins {
+		d := dist(i, point{0, 0})
+		if d < min {
+			min = d
+		}
+	}
+	fmt.Println(min)
+}
+
+func part2(lines []string) {
+	w1 := wire(lines[0])
+	w2 := wire(lines[1])
+
+	ins := intersect(w1, w2)
+
+	min := math.MaxInt32
+
+	for _, i := range ins {
+		v := stepsTo(i, w1) + stepsTo(i, w2)
+		if v < min {
+			min = v
+		}
+	}
+
+	fmt.Println(min)
+
+}
+
+type point struct{ x, y int }
+
+func stepsTo(p point, w []point) int {
+	for i, wp := range w {
+		if wp.x == p.x && wp.y == p.y {
+			return i + 1
+		}
+	}
+	panic("not here")
+}
+
+func dist(p1, p2 point) int {
+	return abs(p1.x-p2.x) + abs(p1.y-p2.y)
+}
+
+func abs(i int) int {
+	if i < 0 {
+		return -i
+	}
+	return i
+}
+
+func hash(p point) string {
+	return fmt.Sprintf("x:%d,y:%d", p.x, p.y)
+}
+
+func intersect(w1, w2 []point) []point {
+	ret := []point{}
+	m := map[string]bool{}
+	for _, p1 := range w1 {
+		m[hash(p1)] = true
+	}
+	for _, p2 := range w2 {
+		if has, ok := m[hash(p2)]; ok && has {
+			ret = append(ret, p2)
+		}
+	}
+	return ret
+}
+
+func wire(line string) []point {
+	ret := []point{}
+	parse := strings.Split(line, ",")
+
+	cur := point{0, 0}
+	for _, ins := range parse {
+		dir := string(ins[0])
+		dist, err := strconv.Atoi(ins[1:])
+		if err != nil {
+			panic(err)
+		}
+
+		var n point
+		for i := 1; i <= dist; i++ {
+			switch dir {
+			case "R":
+				n = point{cur.x + i, cur.y}
+			case "L":
+				n = point{cur.x - i, cur.y}
+			case "U":
+				n = point{cur.x, cur.y + i}
+			case "D":
+				n = point{cur.x, cur.y - i}
+			default:
+				panic("Unknown dir")
+			}
+			ret = append(ret, n)
+		}
+		cur = n
+	}
+	return ret
+}
